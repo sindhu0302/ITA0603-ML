a94f804270432f8f756d1bda8a78fbd0d4580eed
sindhu0302 <192424197.simats@saveetha.com>
2026-08-17 14:57:01 +0530

    ml7.5
    Replaced Linear Regression model with Logistic Regression for employee promotion prediction.

--- DAILY PROGRAMS/ml7.5.py
+++ DAILY PROGRAMS/ml7.5.py
@@ -1,27 +1,15 @@
-from sklearn.linear_model import LinearRegression
-import pandas as pd
+# 7.4 Logistic Regression - Employee Promotion
 
-data = {
-    'EngineCapacity': [1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8],
-    'Weight': [900,950,1050,1150,1250,1350,1450,1550,1650,1750],
-    'Speed': [60,70,80,90,100,110,120,130,140,150],
-    'Mileage': [24,22,20,18,16,15,13,12,11,10]
-}
+from sklearn.linear_model import LogisticRegression
 
-df = pd.DataFrame(data)
+X = [
+    [10,95,50],[8,90,45],[7,88,40],[3,65,20],[2,60,18],
+    [9,93,48],[4,70,25],[8,89,42],[2,58,15],[6,85,38]
+]
 
-X = df[['EngineCapacity', 'Weight', 'Speed']]
-y = df['Mileage']
+y = [1,1,1,0,0,1,0,1,0,1]
 
-model = LinearRegression()
+model = LogisticRegression()
 model.fit(X, y)
 
-new_data = pd.DataFrame({
-    'EngineCapacity': [1.5],
-    'Weight': [1100],
-    'Speed': [85]
-})
-
-prediction = model.predict(new_data)
-
-print("Predicted Mileage:", round(prediction[0], 2), "km/L")
+print("Predicted Class:", model.predict([[7,90,44]]))
