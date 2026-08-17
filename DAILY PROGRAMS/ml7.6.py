201e5cd81fb8d6dee52cb43d69786fbd79bce524
sindhu0302 <192424197.simats@saveetha.com>
2026-08-17 14:57:55 +0530

    ml7.6

--- DAILY PROGRAMS/ml7.6.py
+++ DAILY PROGRAMS/ml7.6.py
@@ -1,27 +1,9 @@
-from sklearn.linear_model import LinearRegression
-import pandas as pd
+from sklearn.linear_model import LogisticRegression
 
-data = {
-    'AdvertisingCost': [10,15,20,25,30,35,40,45,50,55],
-    'Salespersons': [2,3,4,5,6,7,8,9,10,11],
-    'StoreVisits': [120,150,180,210,240,260,280,300,320,340],
-    'MonthlySales': [3.5,4.5,5.8,7.0,8.2,9.5,10.8,12.0,13.5,15.0]
-}
+X = [[1],[2],[3],[6],[7],[8]]
+y = [0,0,0,1,1,1]
 
-df = pd.DataFrame(data)
-
-X = df[['AdvertisingCost', 'Salespersons', 'StoreVisits']]
-y = df['MonthlySales']
-
-model = LinearRegression()
+model = LogisticRegression()
 model.fit(X, y)
 
-new_data = pd.DataFrame({
-    'AdvertisingCost': [38],
-    'Salespersons': [7],
-    'StoreVisits': [270]
-})
-
-prediction = model.predict(new_data)
-
-print("Predicted Monthly Sales:", round(prediction[0], 2), "Lakhs")
+print("Prediction:", model.predict([[5]]))
